3773c8a9c9acb0845e2a6ab27e5e881972f1d621
Kennetor <kenneth.toer@gmail.com>
2026-03-03 02:39:38 +0100

    huehue

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold ">Welcome to your dashboard!</h1><br></br>
-        <h3 className='text-3xl font-bold underline'>Coming soon..</h3>
-        </div>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Welcome to your dashboard!</h1>
+
+      <h3 className="text-3xl font-bold underline">
+        Coming{" "}
+        <span className="relative inline-block group">
+          <span className="underline group-hover:opacity-0">
+            soon
+          </span>
+          <span className="absolute left-0 top-0 underline hover:scale-900 hover:text-[oklch(70.4%_0.191_22.216)] opacity-0 group-hover:opacity-100">
+            Kessaya
+          </span>
+        </span>
+        ..
+      </h3>
+    </div>
     </>
   )
 }
